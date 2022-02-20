5bbba11a736559c2af58f2e2005a479bb5bf7060
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-02-20 17:56:44 -0600

    first commit

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'her-form', 
+    templateUrl: 'hero-form.component.html', 
+    styleUrls: ['hero-form.component.css']
+})
+
+export class HeroFormComponent {
+    title:string = 'Hero-Form';
+
+    
+}
