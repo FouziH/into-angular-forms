34456c7333e392b3bc6407db0cbeca9dce5f5ba1
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-02-20 18:29:45 -0600

    ....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { HeroFormComponent } from './hero-form/hero-form.component';
-import { FormControl } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,8 +12,9 @@ import { FormControl } from '@angular/forms';
     HeroFormComponent
   ],
   imports: [
-    BrowserModule, 
-    FormControl
+    BrowserModule,
+    FormsModule,
+     ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
