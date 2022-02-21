1c2b9df25dea82dc38e52c99c9f6f0d9c4d78322
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-02-20 19:17:49 -0600

    ...

--- src/app/profile-editor/profile-edit.component.ts
+++ src/app/profile-editor/profile-edit.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
-    selector: 'app-profile-editor',
-    templateUrl: 'profile-edit.component.html',
-    styleUrls: ['profile-edit.component.css']
+  selector: 'app-profile-editor',
+  templateUrl: 'profile-edit.component.html',
+  styleUrls: ['profile-edit.component.css'],
 })
-
 export class ProfileEditorComponent {
-    
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+  });
 }
