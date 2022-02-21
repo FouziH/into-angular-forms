11001543de76bb3b09ed30180c7cbb351243999f
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-02-21 08:01:07 -0600

    ....

--- src/app/profile-editor/profile-edit.component.ts
+++ src/app/profile-editor/profile-edit.component.ts
@@ -6,15 +6,61 @@ import { FormArray } from '@angular/forms';
 @Component({
   selector: 'app-profile-editor',
   templateUrl: 'profile-edit.component.html',
-  styleUrls: ['profile-edit.component.css'],
+  styles:[
+      `
+       form {
+        padding-top: 1rem
+    }
+    
+    label{
+        display: block;
+        margin: .5rem 0;
+        font-weight: bold;
+
+
+    }
+    
+    `
+  ] 
+  
 })
 export class ProfileEditorComponent {
-  profileForm = this.fb.group({
-    firstName: ['', Validators.required],
-  });
+    profileForm = this.fb.group({
+        firstName: ['', Validators.required],
+        lastName: [''],
+        address: this.fb.group({
+          street: [''],
+          city: [''],
+          state: [''],
+          zip: ['']
+        }),
+        aliases: this.fb.array([
+          this.fb.control('')
+        ])
+      });
     
-
+      get aliases() {
+        return this.profileForm.get('aliases') as FormArray;
+      }
     
-
+      constructor(private fb: FormBuilder) { }
+    
+      updateProfile() {
+        this.profileForm.patchValue({
+          firstName: 'Nancy',
+          address: {
+            street: '123 Drew Street'
+          }
+        });
+      }
+    
+      addAlias() {
+        this.aliases.push(this.fb.control(''));
+      }
+    
+      onSubmit() {
+        // TODO: Use EventEmitter with form value
+        console.warn(this.profileForm.value);
+      }
 
 }
